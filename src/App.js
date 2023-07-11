ea3262f4dd7f64fc5b8ea87b7ac52b471acb5292
Leo Božić <leobozic2903@gmail.com>
2023-07-11 20:06:48 +0200

    gotovo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+
+const array = [5, 2, 3, 7, 9];
+
+function najveci(arr) {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+function FunctionKomponenta() {
+  return <div>{najveci(array)}</div>;
+}
+
+class ClassKomponenta extends Component {
+  render() {
+    return <div>Obican tekst</div>;
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FunctionKomponenta />
+      <ClassKomponenta />
     </div>
   );
 }
